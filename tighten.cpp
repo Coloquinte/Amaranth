64c09787cfbd5538cec532ffef126e2ae9afb318
Gabriel Gouvine <gabriel.gouvine_GIT@m4x.org>
2015-07-03 09:59:49 +0200

    Basic tightening (redundant with the relaxation)

--- tighten.cpp
+++ tighten.cpp
@@ -15,6 +15,16 @@ int round_upper(int pos, int pitch){
 }
 
 void placement_problem::tighten(){
+    // Tighten based on fixed position: doesn't improve on the minimum-cost-flow relaxation in itself
+    for(relative_constraint constraint : x_constraints){
+        position_constraints[constraint.sc].xmin = std::max(position_constraints[constraint.sc].xmin, position_constraints[constraint.fc].xmin + constraint.min_dist);
+        position_constraints[constraint.fc].xmax = std::min(position_constraints[constraint.fc].xmax, position_constraints[constraint.sc].xmax - constraint.min_dist);
+    }
+    for(relative_constraint constraint : y_constraints){
+        position_constraints[constraint.sc].ymin = std::max(position_constraints[constraint.sc].ymin, position_constraints[constraint.fc].ymin + constraint.min_dist);
+        position_constraints[constraint.fc].ymax = std::min(position_constraints[constraint.fc].ymax, position_constraints[constraint.sc].ymax - constraint.min_dist);
+    }
+
     // Tighten based on the pitches for the cells
     for(int i=0; i<cells.size(); ++i){
         rect & cur = position_constraints[i];
@@ -25,9 +35,10 @@ void placement_problem::tighten(){
     }
 
     // Tighten based on the other cells
+    // Simple transitivity of the order is handled by the MCF relaxation: we need something better
     // A simple approximation is area-based: limited by the area taken by cells constrained to stay on each side
     
-    
+    // Tighten based on feasible regions
 }
 
 
